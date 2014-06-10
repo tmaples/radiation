43474a5085b74a183af8bf5016c300538d49b0b6
Thomas Maples <tmaples90@gmail.com>
2014-06-10 12:20:49 -0500

    edited outputError.py

--- Python/outputError.py
+++ Python/outputError.py
@@ -1,35 +1,29 @@
 from radiation import *
-from scipy.stats.stats import pearsonr
 import globals
 
-outputFileName = globals.projectDirectory + 'error/correlations.csv'
+outputFilePath = globals.projectDirectory + 'error2/'
 
 defineModel(['us'],['us'])
 calculateRadiusPopulations()
 loadCommuters()
 
-def outputError(parameter):
+def outputError(parameter, formulation):
 	rows = getRows(dataCompatible=True)
 	columns = getColumns(dataCompatible=True)
 	
-	model = fluxMatrix(dataCompatible=True, parameter=parameter, round=False)
+	model = fluxMatrix(dataCompatible=True, parameters=parameter, round=True, formulation=formulation)
 	data = commuterMatrix()
 	error = np.log(data + 1) - np.log(model + 1)
-	distance = distanceMatrix()
-	output = ['source,cor,p-value']
 	
 	for i in range(len(rows)):
-		sourceUid = rows[i]
-		errorList = []
-		distanceList = []
+		sourceUid = rows[i][1:]
+		outputFileName = outputFilePath + sourceUid + '.csv'
+		output = ['uid,error']
 		for j in range(len(columns)):
-			destUid = columns[j]
-			if sourceUid == destUid:
+			destUid = columns[j][1:]
+			if sourceUid == destUid or error[i][j] == 0:
 				continue
-			errorList.append(error[i][j])
-			distanceList.append(distance[i][j])
-		cor, p = pearsonr(errorList, distanceList)
-		output.append(','.join([sourceUid, str(cor), str(p)]))
-	open(outputFileName, 'w').write('\n'.join(output))
+			output.append(','.join([destUid, str(error[i][j])]))
+		open(outputFileName, 'w').write('\n'.join(output))
 
-outputError(None)
+outputError(None, 'Standard')
